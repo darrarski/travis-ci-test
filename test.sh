a067b7ec499b81657793faadddd4499faef7ec5f
Dariusz Rybicki <darrarski@gmail.com>
2014-10-24 00:47:37 +0200

    test for iTMSTransporter

---
+++ test.sh
@@ -0,0 +1,5 @@
+which iTMSTransporter
+
+ls -la /Applications/Xcode.app/Contents/Applications/Application\ Loader.app/Contents/MacOS/itms/bin
+
+/Applications/Xcode.app/Contents/Applications/Application\ Loader.app/Contents/MacOS/itms/bin/iTMSTransporter --help
